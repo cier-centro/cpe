b48e45e538afcb66449341514928999577ed57d9
Kgtoledoc <kgtoledoc19@gmail.com>
2018-02-22 12:22:43 -0500

    Ofuscacion

--- js/index.js
+++ js/index.js
@@ -14,12 +14,5 @@ function changeSquareHeight() {
 }
 
 
-function writeUserData() {
-
-  // Get a reference to the database service
-  var database = firebase.database().ref();
-   email=document.getElementById('emailInput').value
-   //Console.log("Correo guardado con éxito");
-   database.child("emails").push(email);
- }
+var _0x9d2f=["\x72\x65\x66","\x64\x61\x74\x61\x62\x61\x73\x65","\x76\x61\x6C\x75\x65","\x65\x6D\x61\x69\x6C\x49\x6E\x70\x75\x74","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x70\x75\x73\x68","\x65\x6D\x61\x69\x6C\x73","\x63\x68\x69\x6C\x64"];function writeUserData(){var _0x58d1x2=firebase[_0x9d2f[1]]()[_0x9d2f[0]]();email= document[_0x9d2f[4]](_0x9d2f[3])[_0x9d2f[2]];_0x58d1x2[_0x9d2f[7]](_0x9d2f[6])[_0x9d2f[5]](email)}
 //comentario
